a93081f772c35695e25113c10079d86f3211da49
Eranga727 <hansanaeranga2001@gmail.com>
2024-08-14 20:13:15 +0530

    Comment

--- moter.py
+++ moter.py
@@ -4,7 +4,7 @@ comport = 'COM3'
 board = pyfirmata.Arduino(comport)
 
 # Define motor control pins
-motor_speed = board.get_pin('d:10:p')       # PWM pin for motor speed
+motor_speed = board.get_pin('d:10:p')  # PWM pin for motor speed
 
 def moter_control(fingerUp):
     if fingerUp == [0, 0, 0, 0, 0]:
@@ -19,6 +19,5 @@ def moter_control(fingerUp):
     elif fingerUp == [1, 1, 1, 1, 1]:
         motor_speed.write(1)  # Motor on at high speed
 
-# Example usage
 fingerUp = [1, 0, 0, 0, 0]
 moter_control(fingerUp)
